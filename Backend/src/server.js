c55918779572248667b4d9a14bfe38708c0d57b3
Yuvraj Alone <yuvrajalone12@gmail.com>
2026-08-26 03:00:56 +0530

    refactor(auth): update route protection

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,15 +4,17 @@ import {connectDB} from "./lib/db.js";
 import cors from "cors";
 import {serve} from "inngest/express";
 import {inngest , functions} from "./lib/inngest.js";
+import {clerkMiddleware} from '@clerk/express';
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({origin: ENV.CLIENT_URL, credentials:true}));
-
+app.use(clerkMiddleware());
 app.use("/api/inngest", serve({client: inngest, functions}));
 
-app.get("/", (req,res) =>{
+app.get("/", protectRoute, (req,res) =>{
     res.status(200).json("Done")
 });
 
