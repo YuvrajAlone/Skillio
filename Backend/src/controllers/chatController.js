068b680f20bba00b493389d5c9e975b8f55b4464
Yuvraj Alone <yuvrajalone12@gmail.com>
2026-08-26 06:06:27 +0530

    feat: Stream token generation

---
+++ Backend/src/controllers/chatController.js
@@ -0,0 +1,14 @@
+import { chatClient } from "../lib/stream.js";
+
+export async function getStreamToken(req,res) {
+    try {
+        const token = chatClient.createToken(req.user.clerkId)
+        res.status(200).json({
+            token,
+            userId: req.user.clerkId,
+            userName: req.user.name
+        })
+    } catch (error) {
+        
+    }
+}
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import {serve} from "inngest/express";
 import {inngest , functions} from "./lib/inngest.js";
 import {clerkMiddleware} from '@clerk/express';
-import { protectRoute } from "./middleware/protectRoute.js";
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
@@ -13,8 +13,9 @@ app.use(express.json());
 app.use(cors({origin: ENV.CLIENT_URL, credentials:true}));
 app.use(clerkMiddleware());
 app.use("/api/inngest", serve({client: inngest, functions}));
+app.use("/api/chat", chatRoutes);
 
-app.get("/", protectRoute, (req,res) =>{
+app.get("/", (req,res) =>{
     res.status(200).json("Done")
 });
 
