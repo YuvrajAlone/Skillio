144e1dd1bb275537dee5cd47d79554d22c25ade2
Yuvraj Alone <yuvrajalone12@gmail.com>
2026-08-31 02:43:17 +0530

    chore: initialize frontend tools

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,14 @@
-import {  Show, SignInButton, UserButton } from '@clerk/react'
-import './App.css'
-
+import { Show, SignInButton, UserButton, useUser } from "@clerk/react";
+import { Route, Routes } from "react-router";
+import Homepage from "./Pages/Homepage.jsx";
 
 function App() {
-
+  const { isSignedIn } = useUser();
   return (
-    <>
-      <h1>yeahh it's your first project</h1>
-      <Show when="signed-out">
-         <SignInButton mode='modal'>
-          <button>Login</button>
-         </SignInButton>
-      </Show>
-      <Show when="signed-in">
-         <UserButton/>
-      </Show>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ClerkProvider } from '@clerk/react'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ClerkProvider } from "@clerk/react";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider>
-      <App />
-    </ClerkProvider> 
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ClerkProvider>
+          <App />
+        </ClerkProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
