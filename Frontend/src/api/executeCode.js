531b57a6c6ae5c29bd66dff2e480cee9ac384135
Yuvraj Alone <yuvrajalone12@gmail.com>
2026-09-12 02:31:57 +0530

    fix: auth token

--- Frontend/src/api/executeCode.js
+++ Frontend/src/api/executeCode.js
@@ -1,11 +1,15 @@
+import { useAuth } from "@clerk/clerk-react";
+const { getToken } = useAuth();
+const token = await getToken();
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 export async function executeCode(language, code) {
   const response = await fetch(`${API_URL}/api/code/execute`, {
     method: "POST",
-    credentials: "include",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({
       language,
